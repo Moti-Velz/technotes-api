3b57eab9a94ab659cbd6da40b434e1cddda83f9a
Tim <roppa070@gmail.com>
2023-05-06 16:15:31 -0400

    explicit domain on cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,8 @@ const login = async (req, res) => {
         httpOnly: true, //accessible only by web server 
         secure: true, //https
         sameSite: 'None', //cross-site cookie 
-        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
+        maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
+        domain: '.onrender.com'
     })
 
     // Send accessToken containing username and roles 
