76647d0e102ec3851c42e6d1add85285b7656347
Tim <roppa070@gmail.com>
2023-05-06 16:10:32 -0400

    cookie domain changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,8 +44,7 @@ const login = async (req, res) => {
         httpOnly: true, //accessible only by web server 
         secure: true, //https
         sameSite: 'None', //cross-site cookie 
-        maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
-        domain: 'technotes2.onrender.com'
+        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
     })
 
     // Send accessToken containing username and roles 
